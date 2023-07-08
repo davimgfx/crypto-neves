05c4e4de1d63cc553eea810c2c8ace6e060cea6b
Davi Costa <davifncosta@gmail.com>
2023-07-08 10:27:58 -0300

    add the sidebar

--- src/components/Authentication/UserSidebar.jsx
+++ src/components/Authentication/UserSidebar.jsx
@@ -1,18 +1,65 @@
-import React from 'react'
-import { CryptoState } from '../../CryptoContext';
-import { Box } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-const UserSidebar = () => {
- const { user } = CryptoState();
- console.log(user)
-  return (
-    <Box  sx={{
-        marginRight: "1rem",
-        marginY: "auto"
-      }}>
-        {user?.photoURL ? (<img src={user.photoURL}/>) : (<AccountCircleIcon width="90"/>)}
-    </Box>
-  )
-}
+import { Avatar, Box } from "@mui/material";
+import { CryptoState } from "../../CryptoContext";
+import Drawer from "@mui/material/Drawer";
+import * as React from "react";
 
-export default UserSidebar
+export default function UserSidebar() {
+  const { user } = CryptoState();
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  return (
+    <div>
+      {["right"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          {user?.photoURL ? (
+            <img
+              src={user.photoURL}
+              alt="profile_photo"
+              onClick={toggleDrawer(anchor, true)}
+            />
+          ) : (
+            <Avatar
+              onClick={toggleDrawer(anchor, true)}
+              sx={{
+                marginRight: "1rem",
+                cursor: "pointer",
+                background: " rgba(144,202,249,1) ",
+                color: "#292B2F",
+                width: 32,
+                height: 32,
+              }}
+            />
+          )}
+
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}>
+            <Box sx={{
+              width: 350,
+              padding: 25,
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}>
+
+            </Box>
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
